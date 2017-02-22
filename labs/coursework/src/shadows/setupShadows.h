54c25303025cf764005b9076f127ba6d492f8be3
Davide Morello <eloh666@gmail.com>
2017-02-22 12:54:11 +0000

    some bg work for shadows

--- labs/coursework/src/shadows/setupShadows.h
+++ labs/coursework/src/shadows/setupShadows.h
@@ -5,12 +5,34 @@ using namespace std;
 using namespace graphics_framework;
 using namespace glm;
 
-void updateShadowPostions(vector<shadow_map> &shadows, vector<point_light> &points, vector<spot_light> &spots)
+effect createShadowEffect()
 {
-	
+
+	effect eff;
+	eff.add_shader("shaders/basic_spotLight.vert", GL_VERTEX_SHADER);
+	eff.add_shader("shaders/basic_spotLight.frag", GL_FRAGMENT_SHADER);
+	eff.build();
+	return eff;
 }
 
-void getLightProjectionMatrices(vector<mat4> &lightMVPs, vector<shadow_map> &shadows, mat4 lightProjection, mat4 meshModel)
+// Calculates the shadows based on the positions of the spotlights
+// This naive version does not yet take in consideration point lights
+void updateShadowPostions(vector<shadow_map> &shadows, vector<spot_light> &spots)
 {
-	
+	for (auto i = 0; i < spots.size(); ++i)
+	{
+		shadows[i].light_dir = spots[i].get_direction();
+		shadows[i].light_position = spots[i].get_position();
+	}
+}
+
+// Computes the light mvp matrix based on the number of shadows
+vector<mat4> getLightProjectionMatrices(vector<shadow_map> &shadows, mat4 lightProjection, mat4 meshModel)
+{
+	vector<mat4> lightMVPs(shadows.size());
+	for (auto i = 0; i < lightMVPs.size(); ++i)
+	{
+		lightMVPs[i] = lightProjection * shadows[i].get_view() * meshModel;
+	}
+	return lightMVPs;
 }
